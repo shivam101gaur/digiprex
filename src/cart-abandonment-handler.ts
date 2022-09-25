3c241a51fc17c40f89b7236b618d6f7696b6cb60
shivam101gaur <gaurs3862@gmail.com>
2022-09-25 17:33:12 +0530

    .

--- src/cart-abandonment-handler.ts
+++ src/cart-abandonment-handler.ts
@@ -1,7 +1,13 @@
-export function handleAbaondment(params: handleAbaondmentBody) {
 
+export function handleAbaondment(params: handleAbaondmentBody) {
+    
 }
 
 export type handleAbaondmentBody = {
-    
+    c_name: string;
+    id: number;
+    phone: string;
+    email: string;
+    cart_token:string;
+    time_of_abandonment:number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import express, { Express, Request, Response } from 'express';
+import { handleAbaondment, handleAbaondmentBody } from './cart-abandonment-handler';
+import { PostCartAbandonedBody } from './models/postCartAbandoned.model';
 
-const app:Express = express();
+const app: Express = express();
 const port = 8000
 
 
 
 app.get('/', (req: Request, res: Response) => {
-  
+
   res.send('DIGIPREX SERVER IS LIVE ! ⚡');
 });
 
-app.post('/cart-abandoned',(req:Request,res:Response)=>{
+app.post('/cart-abandoned', (req: Request, res: Response) => {
   console.log("A new request received at " + Date.now());
 })
-app.post('/order-created',(req:Request,res:Response)=>{
-  
+app.post('/order-created', (req: Request, res: Response) => {
+  const reqBody: PostCartAbandonedBody = req.body;
+  const handleBody: handleAbaondmentBody = { c_name: reqBody.customer.first_name, cart_token: reqBody.cart_token, email: reqBody.customer.email, id: reqBody.id, phone: reqBody.phone.phone, time_of_abandonment: Date.now() }
+  handleAbaondment(handleBody)
+  res.send('handled successfully')
 })
 
 app.listen(port, () => {
